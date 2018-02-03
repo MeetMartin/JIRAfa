5f7825a660bc87e35ce45894976e52e354c310b4
martin.novak <ragnarecek@gmail.com>
2018-02-02 20:32:14 -0600

    Hindley-Milner and converted all to expressions

--- extension/modules/backlog-compacter.js
+++ extension/modules/backlog-compacter.js
@@ -4,17 +4,15 @@ import {onBacklogDrawn, onBacklogUpdated} from './jira-event-manager.js';
 
 /**
  * Returns the end element of a single backlog issue
- * @impure well... JQuery
  * @param {JQuery} issue JIRA .js-issue node
- * @returns {JQuery} JIRA .ghx-end element inside of issue
+ * @return {JQuery} findTheEndOfIssue :: JQuery -> JQuery
  */
 const findTheEndOfIssue = issue => issue.find ('.ghx-end');
 
 /**
  * Finds extra fields content of a single issue, compacts it and prepends to a target element
- * @impure well... JQuery
  * @param {JQuery} issue JIRA .js-issue node
- * @returns {JQuery} augmented issue
+ * @return {JQuery} compactAndMoveExtraFieldsContent :: JQuery -> JQuery
  */
 const compactAndMoveExtraFieldsContent = issue =>
     issue.find ('.ghx-extra-field-content')
@@ -24,18 +22,16 @@ const compactAndMoveExtraFieldsContent = issue =>
     && issue;
 /**
  * Finds Epic and Version elements and prepends to a target element
- * @impure well... JQuery
  * @param {JQuery} issue JIRA .js-issue node
- * @returns {JQuery} augmented issue
+ * @return {JQuery} moveEpicAndVersion :: JQuery -> JQuery
  */
 const moveEpicAndVersion = issue =>
     issue.find ('.ghx-end .aui-label').prependTo (findTheEndOfIssue (issue)) && issue;
 
 /**
  * Finds extra fields, appends them to a target element and compacts the issue
- * @impure well... JQuery
  * @param {JQuery} issue JIRA .js-issue element
- * @returns {JQuery} augmented issue
+ * @return {JQuery} moveExtraFieldsAndCompactIssue :: JQuery -> JQuery
  */
 const moveExtraFieldsAndCompactIssue = issue => {
     const extraFields = issue.find ('.ghx-plan-extra-fields');
@@ -46,26 +42,23 @@ const moveExtraFieldsAndCompactIssue = issue => {
 
 /**
  * Removes extra items with None value
- * @impure well... JQuery
  * @param {JQuery} backlogIssues backlog issues where we should perform the changes
- * @returns {JQuery} backlogIssues without extra items
+ * @return {JQuery} removeExtraItemsWithNoneValue :: JQuery -> JQuery
  */
 const removeExtraItemsWithNoneValue = backlogIssues =>
     backlogIssues.find ('span.ghx-extra-field-content:contains(None)').remove () && backlogIssues;
 
 /**
  * Marks issue as processed by adding css class JIRAfaCompacted
- * @impure well... JQuery
  * @param {JQuery} issue issue JIRA .js-issue element
- * @returns {JQuery} augmented issue
+ * @return {JQuery} markIssueAsProcessed :: JQuery -> JQuery
  */
 const markIssueAsProcessed = issue => issue.addClass ('JIRAfaCompacted');
 
 /**
  * Processes a single backlog issue making it more compact
- * @impure well... JQuery
  * @param {JQuery} issue issue JIRA .js-issue element
- * @returns {JQuery} augmented issue
+ * @return {JQuery} compactIssue :: JQuery -> JQuery
  */
 const compactIssue = issue => composePipe (
     moveEpicAndVersion,
@@ -75,22 +68,23 @@ const compactIssue = issue => composePipe (
 ) (issue);
 
 /**
- * Compacts UI of all issues in backlog so they are displayed as one line
- * @impure well... JQuery
- * @returns {JQuery} Promise with JQuery elements of .js-issue:not(.JIRAfaCompacted)
+ * Gets JQuery of backlog issues to process
+ * @return {JQuery} issuesToProcess :: () -> JQuery
  */
-const compactBacklogIssues = () => {
-    const backlogIssues = $ ('.js-issue:not(.JIRAfaCompacted)');
-    if (backlogIssues.length > 0) {
-        removeExtraItemsWithNoneValue (backlogIssues).each ((index, issue) => compactIssue ($ (issue)));
-        log (backlogIssues.length + ' of issues was compacted in backlog.');
-    }
-    return backlogIssues;
-};
+const issuesToProcess = () => $ ('.js-issue:not(.JIRAfaCompacted)');
+
+/**
+ * Compacts UI of all issues in backlog so they are displayed as one line
+ * @return {JQuery} compactBacklogIssues :: () -> JQuery
+ */
+const compactBacklogIssues = () => (issues => issues.length > 0 ?
+    log (issues.length + ' of issues was compacted in backlog.') &&
+        removeExtraItemsWithNoneValue (issues).each ((index, issue) => compactIssue ($ (issue))) &&
+            issues :
+    issues) (issuesToProcess ());
 
 /**
  * Makes backlog issues always compact
- * @impure well... JQuery
  * @returns {compactBacklogIssues} makeBacklogIssuesAlwaysCompact :: null -> (a -> b)
  */
 const makeBacklogIssuesAlwaysCompact = () =>
