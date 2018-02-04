3ed10ce23cd88643c6d70465fb7303e68129a545
martin.novak <ragnarecek@gmail.com>
2018-02-03 18:48:20 -0600

    initial test setup with jsdom

---
+++ tests/backlog-compacter.test.js
@@ -0,0 +1,28 @@
+import assert from 'assert';
+import proxy from 'proxyquire';
+import {JSDOM} from'jsdom';
+
+import logger from "./mocks/modules/logger.mock";
+import eventManager from './mocks/modules/event-manager.mock.js';
+
+proxy.noCallThru ();
+const Test = proxy ('../extension/modules/backlog-compacter.js',
+    { '../utilities/logger.js': logger, './event-manager.js': eventManager });
+
+beforeEach (() => {
+    global.window = new JSDOM (`<!DOCTYPE html><p>Hello world</p>`).window;
+    global.document = global.window.document;
+});
+
+afterEach (() => {
+    delete global.window;
+    delete global.document;
+});
+
+describe ('Backlog Compacter', () => {
+    describe ('makeBacklogIssuesAlwaysCompact', () => {
+        it ('should return a function', () => {
+            assert.deepStrictEqual (typeof Test.makeBacklogIssuesAlwaysCompact (), 'function');
+        });
+    });
+});
