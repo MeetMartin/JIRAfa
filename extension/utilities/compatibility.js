936f8428a7a7280869ace9b4c581b824a9e4b283
Martin Novak <martin.novak@sm-laptop-049.soulmachines.com>
2018-09-18 12:06:01 +1200

    GH object compatibility testing added

--- extension/utilities/compatibility.js
+++ extension/utilities/compatibility.js
@@ -5,12 +5,23 @@
 const isGHAvailable = () => typeof GH !== 'undefined';
 
 /**
+ * Returns boolean whether JIRA GrassHopper object is compatible with JIRAfa extension.
+ * @return {Boolean} isGHCompatible :: () -> Boolean
+ */
+const isGHCompatible = () =>
+    typeof GH.PlanController.show !== 'undefined' &&
+    typeof GH.BacklogView.draw !== 'undefined' &&
+    typeof GH.PlanDragAndDrop.enableDragAndDrop !== 'undefined' &&
+    typeof GH.WorkController.setPoolData !== 'undefined';
+
+/**
  * Returns boolean whether JIRA is compatible with JIRAfa extension.
  * @return {Boolean} isJIRACompatible :: () -> Boolean
  */
-const isJIRACompatible = () => isGHAvailable ();
+const isJIRACompatible = () => isGHAvailable () && isGHCompatible ();
 
 export {
     isGHAvailable,
+    isGHCompatible,
     isJIRACompatible
 }
--- tests/compatibility.test.js
+++ tests/compatibility.test.js
@@ -4,7 +4,8 @@ import {getGH} from './mocks/grass-hopper.mock.js';
 
 import {
     isGHAvailable,
-    isJIRACompatible
+    isJIRACompatible,
+    isGHCompatible
 } from '../extension/utilities/compatibility.js';
 
 beforeEach (() => {
@@ -20,18 +21,59 @@ describe ('Jira Compatibility Tester', () => {
         it ('should be true if GrassHopper object is avaialble', () => {
             assert.deepStrictEqual (isGHAvailable (), true);
         });
+
         it ('should be false if GrassHopper object is missing', () => {
             delete global.GH;
             assert.deepStrictEqual (isGHAvailable (), false);
         });
     });
+
     describe ('isJIRACompatible', () => {
         it ('should be true if all functions testing JIRA compatibility return true', () => {
             assert.deepStrictEqual (isJIRACompatible (), true);
         });
+
         it ('should be false if GH object is missing', () => {
             delete global.GH;
             assert.deepStrictEqual (isJIRACompatible (), false);
+        });
+    });
+
+    describe ('isGHCompatible', () => {
+        it ('should be true if GH.PlanController.show is defined', () => {
+            assert.deepStrictEqual (isGHCompatible (), true);
+        });
+
+        it ('should be false if GH.PlanController.show is not defined', () => {
+            delete global.GH.PlanController.show;
+            assert.deepStrictEqual (isGHCompatible (), false);
+        });
+
+        it ('should be true if GH.BacklogView.draw is defined', () => {
+            assert.deepStrictEqual (isGHCompatible (), true);
+        });
+
+        it ('should be false if GH.BacklogView.draw is not defined', () => {
+            delete global.GH.BacklogView.draw;
+            assert.deepStrictEqual (isGHCompatible (), false);
+        });
+
+        it ('should be true if GH.PlanDragAndDrop.enableDragAndDrop is defined', () => {
+            assert.deepStrictEqual (isGHCompatible (), true);
+        });
+
+        it ('should be false if GH.PlanDragAndDrop.enableDragAndDrop is not defined', () => {
+            delete global.GH.PlanDragAndDrop.enableDragAndDrop;
+            assert.deepStrictEqual (isGHCompatible (), false);
+        });
+
+        it ('should be true if GH.WorkController.setPoolData is defined', () => {
+            assert.deepStrictEqual (isGHCompatible (), true);
+        });
+
+        it ('should be false if GH.WorkController.setPoolData is not defined', () => {
+            delete global.GH.WorkController.setPoolData;
+            assert.deepStrictEqual (isGHCompatible (), false);
         });
     });
 });
