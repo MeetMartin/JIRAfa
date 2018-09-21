48c16eba6185275f40359a9af4b0fb365d6172b3
Martin Novak <martin.novak@sm-laptop-049.soulmachines.com>
2018-09-21 14:34:04 +1200

    fixed sprint button

--- extension/modules/sprints-button.js
+++ extension/modules/sprints-button.js
@@ -2,19 +2,28 @@ import {createDropdown} from './button.js';
 import {onBacklogUpdated} from './event-manager.js';
 
 /**
+ * Populates menu with names of sprints leading to the sprints on the board.
+ * @param {JQuery} button
+ * @return {JQuery} populateMenu :: JQuery -> () -> JQuery
+ */
+const populateMenu = button => () => {
+    button.clearMenu ();
+    $ ('.js-sprint-header .header-left .ghx-name').each ((index, element) => {
+        button.addMenuOption ($ (element).text (), () => element.scrollIntoView ());
+    });
+    button.addMenuOption ('Backlog', () => $ ('div.js-marker-backlog-header').get (0).scrollIntoView ());
+    return button;
+};
+
+/**
  * Creates jump to sprint button to quickly navigate among sprints in backlog
- * @returns {JQuery} jump to sprint button
+ * @returns {JQuery} createSprintsButton :: () -> JQuery
  */
 const createSprintsButton = () => {
     const button = createDropdown ('Sprints');
     button.addMenuOption ('Loading...', () => null);
-    onBacklogUpdated (() => {
-        button.clearMenu ();
-        $ ('span[data-fieldname=sprintName]').each ((index, element) => {
-            button.addMenuOption ($ (element).attr ('data-fieldvalue'), () => element.scrollIntoView ());
-        });
-        button.addMenuOption ('Backlog', () => $ ('div.js-marker-backlog-header').get (0).scrollIntoView ());
-    });
+    populateMenu (button) ();
+    onBacklogUpdated (populateMenu (button));
     return button;
 };
 
